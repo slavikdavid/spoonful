03782dc8f4163cb714d378c4ce65a52a0973743d
David Slavik <dslavik20@foi.hr>
2025-09-22 08:56:43 +0200

    initial commit

---
+++ src/main/java/com/spoonful/spoonful/SpoonfulApplication.java
@@ -0,0 +1,13 @@
+package com.spoonful.spoonful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoonfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spoonful/spoonful/SpoonfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoonful.spoonful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoonfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
